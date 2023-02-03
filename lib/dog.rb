c44fe2d51bd1369a8948c15bbbc463ac990d7e9a
Jessica <andradejsscl@gmail.com>
2023-02-03 10:42:48 -0500

    commit tree d46db07fd19ed62b7aa5d8dae2c95ea95f2c3501

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,64 @@
+require 'pry'
+
 class Dog
+    attr_accessor :id, :name, :breed
+
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = 
+        <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = 
+        <<-SQL
+        DROP TABLE dogs
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT * FROM dogs ORDER BY dogs.id DESC LIMIT 1")[0][0]
+        self
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+    end
+
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+
+    def self.all
+        sql = 
+        <<-SQL
+            SELECT * 
+            FROM dogs;
+        SQL
+        DB[:conn].execute(sql).map do |row|
+            self.new_from_db(row)
+        end
+    end
+
 
 end
+
+# binding.pry
